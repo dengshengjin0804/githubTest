87659b16af73d7e95b2f5acaf3b5d96a05f82d92
dengshengjin <dsj@localhost.(none)>
2012-12-22 15:25:23 +0800

    Initial Commit

---
+++ githubTestTests/githubTestTests.h
@@ -0,0 +1,13 @@
+//
+//  githubTestTests.h
+//  githubTestTests
+//
+//  Created by dengshengjin on 12-12-22.
+//  Copyright (c) 2012年 dengshengjin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface githubTestTests : SenTestCase
+
+@end
---
+++ githubTestTests/githubTestTests.m
@@ -0,0 +1,32 @@
+//
+//  githubTestTests.m
+//  githubTestTests
+//
+//  Created by dengshengjin on 12-12-22.
+//  Copyright (c) 2012年 dengshengjin. All rights reserved.
+//
+
+#import "githubTestTests.h"
+
+@implementation githubTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in githubTestTests");
+}
+
+@end
